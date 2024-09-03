f90c63802a1ab0e81a14e0ebb48000effae8c806
murilokloppel <murilo.kloppel@fatec.sp.gov.br>
2024-09-03 11:19:15 -0300

    add backende 4 json server

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,32 +1,26 @@
-import { Component } from '@angular/core';
-import { product } from '../product';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
 
+  products : Product[] = [];
 
-  products : product[] = [
+  constructor(private service: ProductService){}
 
-    {   "id": 1,
-        "name": "ComputadosI7",
-        "price": 6000.00,
-        "category": "Eletronics"      
-    },
-    {
-      "id": 2,
-        "name": "Camisa GG Branca",
-        "price": 40.00,
-        "category": "Vestuário" 
-    },
-    {
-      "id": 3,
-        "name": "Sorvete de Pistache",
-        "price": 33.45,
-        "category": "Alimentos" 
+  ngOnInit() {
+  this.loadProducts();
+  }
+
+  loadProducts() {
+    this.service.getProducts().subscribe({
+        next: data => this.products = data
     }
-  ];
+    );
+  }
 }
