36e828c8846b9efbef25a526436a43135bde8e0a
murilokloppel <murilo.kloppel@fatec.sp.gov.br>
2024-09-25 01:03:10 -0300

    pop up de alerta

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, numberAttribute, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.css'
 })
-export class ProductFormComponent {
 
+export class ProductFormComponent implements OnInit {
+
+  constructor(private router: Router
+              private activeRoute: ActivatedRoute
+              private service: ProductService
+    ){
+
+  }
+
+  ngOnInit() {
+    const id = Number(this.router.snapshot.paramMap.get("id"))
+    this.loadProduct(id);
+  }
+
+  loadProduct(id: number) {
+       this.service.getProductById(id).subscribe({
+        next: data => alert(data.name)
+       })
+  }
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,7 +13,9 @@ export class ProductService {
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>('http://localhost:3000/products');
   }
-
+  getProductById(id:number): Observable<Product> {
+    return this.http.get<Product>('http://localhost:3000/products/' + id);
+  }
 
   delete(product: Product): Observable<void> {
     return this.http.delete<void>('http://localhost:3000/products/' + product.id);
